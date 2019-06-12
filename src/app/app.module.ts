aa13747623f6af366e419a65be232e7daf289e79
austinbark1 <austinbark1@gmail.com>
2019-06-11 22:01:42 -0400

    changes to fix angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {SignInComponent} from './sign-in/sign-in.component';
 import {FormsModule} from '@angular/forms';
 import {MainViewComponent} from './main-view/main-view.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import {NavbarComponent} from './navbar/navbar.component';
   imports: [
     BrowserModule,
     FormsModule,
-    NgModule
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
