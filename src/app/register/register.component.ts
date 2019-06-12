00f18f22521f590959abc56cd76be565c5a603ed
raznprince <raznprince@gmail.com>
2019-06-11 23:28:39 -0400

    Initial conkstruction of Register Component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,10 +7,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  password: string = '';
+  passwordConfirm: string = '';
+  formsMatch: boolean = false;
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  matchForms = () => {
+    if(this.password != '' && this.passwordConfirm != ''){
+      if(this.password === this.passwordConfirm){
+        this.formsMatch = true;
+      } else this.formsMatch = false;
+    } else {
+      this.formsMatch = false;
+    }
   }
 
 }
