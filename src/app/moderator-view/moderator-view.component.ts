af3454a2e119cf3d7e6b76893da817a18bb0e61e
Stuart Pratuch <stuarttpratuch@gmail.com>
2019-06-13 14:15:04 -0400

    working on html for moderator dashboard

---
+++ src/app/moderator-view/moderator-view.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-moderator-view',
+  templateUrl: './moderator-view.component.html',
+  styleUrls: ['./moderator-view.component.css']
+})
+export class ModeratorViewComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
