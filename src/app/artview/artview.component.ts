7743ca153f4c8bc9f86bad9069970510d91b29a5
Stuart Pratuch <stuarttpratuch@gmail.com>
2019-06-15 23:25:09 -0400

    Created Artviewe component with basic functionality. Still need comment

---
+++ src/app/artview/artview.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artview',
+  templateUrl: './artview.component.html',
+  styleUrls: ['./artview.component.css']
+})
+export class ArtviewComponent implements OnInit {
+
+images=["a","b","c","d","e","f"];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
