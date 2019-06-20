a7d1cf4987c991a8b3ebe8c0b4946c6c8c6fa800
raznprince <raznprince@gmail.com>
2019-06-20 19:27:45 -0400

    updated user model for role and heart fields

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,13 +4,16 @@ export class User{
     firstName: string;
     lastName: string;
     email: string;
+    hearts: string;
+    role: number;
 
-
-  constructor(username: string, password: string, firstName: string, lastName: string, email: string) {
+  constructor(username: string, password: string, firstName: string, lastName: string, email: string, hearts: string, role: number) {
     this.username = username;
     this.password = password;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
+    this.hearts = hearts;
+    this.role = role;
   }
 }
