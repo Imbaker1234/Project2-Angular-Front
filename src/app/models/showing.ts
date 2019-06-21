2c0d1100851d794eab419d0ef9092dd9d44c8a4a
raznprince <raznprince@gmail.com>
2019-06-20 21:04:12 -0400

    showings form

--- src/app/models/showing.ts
+++ src/app/models/showing.ts
@@ -1,11 +1,18 @@
+import { User } from './user';
+
 export class Showing{
+  showId: number;
+  hostId: User;
   description: string;
   time: string;
   address: string;
 
-  constructor(description: string, time: string, address: string) {
+  constructor(showId: number, hostId: User, description: string, time: string, address: string) {
+    this.showId = showId;
+    this.hostId = hostId;
     this.description = description;
     this.time = time;
     this.address = address;
   }
+
 }
--- src/app/showings-form/showings-form.component.ts
+++ src/app/showings-form/showings-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from 'services/state.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-showings-form',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowingsFormComponent implements OnInit {
 
-  constructor() { }
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  description = '';
+  time = '';
+  address = '';
+  reviewDetails = false;
+
+  show = false;
+  reviewCardEffect = 'container animated fadeIn';
+
+  constructor(private stateService: StateService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.firstFormGroup = this.fb.group({
+      firstCtrl: ['', Validators.required]
+    });
+
+    this.secondFormGroup = this.fb.group({
+      secondCtrl: ['', Validators.required]
+    });
+
+    this.thirdFormGroup = this.fb.group({
+      thirdCtrl: ['', Validators.required]
+    });
+
   }
 
+  toggleReviewDetails() {
+    this.reviewDetails = !this.reviewDetails;
+  }
+
+  submitNewShowing() {
+    console.log(this.description);
+    console.log(this.time);
+    console.log(this.address);
+  }
 }
