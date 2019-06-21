ddd5829490714fee5af914e5bdbe3498c402f77a
Imbaker1234 <Imbaker1234@gmail.com>
2019-06-21 01:49:50 -0400

    Updated again to try and fix CORS error

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {StateService} from '../../services/state.service';
 
 @Component({
   selector: 'app-comment',
@@ -7,10 +8,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
+  userRole: number;
   authorName = 'SirBarksALot';
   commentContent = 'I really don\'t think this is appropriate for children.\n\nohno...';
 
-  constructor() { }
+  constructor(private stateService: StateService) {
+    this.userRole = this.stateService.stateUser.role;
+  }
 
   ngOnInit() {
   }
